9a874e4f5211fb294769b8e5313c7815b08b3bf7
Marcelo Figueiredo Garcia <marcelof.garcia@yahoo.com.br>
2023-11-01 15:01:04 -0300

    adiciona mensagens de execução

--- desafio001.sh
+++ desafio001.sh
@@ -2,16 +2,18 @@
 
 #script de criação de estrutura de usuários, diretórios e permissões
 
+echo "criando diretórios..."
+
 mkdir /publico /adm /ven /sec
 chown root /publico /adm /sec
+
+echo "criando grupos de usuários..."
 
 groupadd GRP_ADM 
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-chgrp GRP_ADM /adm
-chgrp GRP_VEN /ven
-chgrp GRP_SEC /sec
+echo "criando usuários e adicionando aos grupos..."
 
 useradd carlos -c "carlos" -m -s /usr/bin/bash -p $(openssl passwd senha123) -G GRP_ADM
 useradd maria -c "maria" -m -s /usr/bin/bash -p $(openssl passwd senha123) -G GRP_ADM
@@ -25,7 +27,15 @@ useradd josefina -c "josefina" -m -s /usr/bin/bash -p $(openssl passwd senha123)
 useradd amanda -c "amanda" -m -s /usr/bin/bash -p $(openssl passwd senha123) -G GRP_SEC
 useradd rogerio -c "rogerio" -m -s /usr/bin/bash -p $(openssl passwd senha123) -G GRP_SEC
 
+echo "configurando permissões dos diretórios..."
+
+chgrp GRP_ADM /adm
+chgrp GRP_VEN /ven
+chgrp GRP_SEC /sec
+
 chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
+
+echo "fim..."
